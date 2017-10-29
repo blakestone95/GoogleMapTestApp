f2ea32fdf4d719d55169edae2bc0a6acfa56596d
Blake Sawyer <blakestone.trumpet1@gmail.com>
2017-10-29 14:55:07 -0700

    Initial commit.  List with map underneath.

---
+++ app/src/main/java/com/example/blake/googlemaptestapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.blake.googlemaptestapp;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private MyMapFragment mapFrag;
+    private MyListFragment listFrag;
+    private FragmentManager manager;
+    private FragmentTransaction txn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create Fragments
+        mapFrag = new MyMapFragment();
+        listFrag = new MyListFragment();
+
+        // Get Transaction
+        manager = getSupportFragmentManager();
+        txn = manager.beginTransaction();
+        txn.add(R.id.text_list, listFrag, "List_Frag");
+        txn.add(R.id.map, mapFrag, "Map_Frag");
+        txn.commit();
+    }
+
+}
---
+++ app/src/main/java/com/example/blake/googlemaptestapp/MyMapFragment.java
@@ -0,0 +1,26 @@
+package com.example.blake.googlemaptestapp;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MyMapFragment extends SupportMapFragment implements OnMapReadyCallback {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
